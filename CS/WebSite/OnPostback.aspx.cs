17c9613204f3fbb8b8dff36721b913db2f5fbb93
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/OnPostback.aspx.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Web.UI;
+using DevExpress.Web.ASPxEditors;
+
+public partial class OnPostback : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
+        hf["IsPostback"] = Page.IsPostBack;
+    }
+}
